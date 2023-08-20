9d63d9174c31d0fd3add5bcd3a435be86206085b
khadija Bidari <bidari.khadija@hotmail.fr>
2023-08-21 01:36:23 +0200

    modif css h1 + modale css js html

--- js/modale.js
+++ js/modale.js
@@ -1,6 +1,37 @@
-const closeButton = document.querySelector('.popup-close');
+// Attend que le DOM soit chargé
+// document.addEventListener('DOMContentLoaded', function() {
+//   const closeButton = document.querySelector('.popup-close');
 
-closeButton.addEventListener('click', function() {
-  // Option 1: Masquer le popup
-  document.querySelector('.popup-overlay').style.display = 'none';
+//   closeButton.addEventListener('click', function() {
+//     // Masque la pop-up en modifiant la propriété de style "display" à "none"
+//     document.querySelector('.popup-overlay').style.display = 'none';
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const modal = document.querySelector('.popup-overlay');
+
+  modal.addEventListener('click', function(event) {
+    if (event.target === modal) {
+      // Masque la pop-up en modifiant la propriété de style "display" à "none"
+      modal.style.display = 'none';
+    }
+  });
 });
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $('.popup-close').click(function() {
+//     // Masque la pop-up en modifiant la propriété de style "display" à "none"
+//     $('.popup-overlay').hide();
+//   });
+// });
+
--- template-parts/modale.php
+++ template-parts/modale.php
@@ -1,11 +1,14 @@
 
 
-<div class="popup-overlay">
+ <div class="popup-overlay">
 	<div class="popup-contact">
-			<span class="popup-close">x</span>
-	</div>
+		<img src=<?php echo get_template_directory_uri() . '/images/contact.png'; ?> alt="contact">
+			<!-- <span class="popup-close">x</span> -->
 		<?php
 		// On insère le formulaire de demandes de renseignements
 		echo do_shortcode('[contact-form-7 id="3116ac9" title="Formulaire de contact"]');
 		?>
-</div>
+	</div>
+	
+		
+</div> 
